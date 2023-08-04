a38945ae6d8969731863a5548396d3d4aea0232e
Daniel Cesar Nunes <nunesdc@hotmail.com>
2023-08-04 16:35:59 -0300

    Implement Interfaces DepartmentDao and SellerDao

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department department);
+	void update(Department department);
+	void deleteById(Department department);
+	Department findById(Integer id);
+	List<Department> findAll();
+	
+}
