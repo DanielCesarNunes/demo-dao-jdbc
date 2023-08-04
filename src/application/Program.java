286dfd2559436774b7cc8929441bc226ddd7c711
Daniel Cesar Nunes <nunesdc@hotmail.com>
2023-08-04 16:10:27 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,11 @@
 package application;
 
-<<<<<<< HEAD
 import java.util.Date;
 import java.util.Locale;
 
 import entities.Department;
 import entities.Seller;
-=======
-import java.util.Locale;
 
-import entities.Department;
->>>>>>> 079cf3056dc1c63ade5c94aec992d05928d76ac5
 
 public class Program {
 
@@ -22,12 +17,11 @@ public class Program {
 		Department department = new Department(1, "GTI");
 		
 		System.out.println(department);
-<<<<<<< HEAD
+
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, department);
 		System.out.println(seller);
-=======
->>>>>>> 079cf3056dc1c63ade5c94aec992d05928d76ac5
+
 	}
 
 }
