e135bc0bdbbb1d49399262dce185ca319d422427
Daniel Cesar Nunes <nunesdc@hotmail.com>
2023-08-04 21:21:00 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import entities.Department;
@@ -17,12 +17,21 @@ public class Program {
 		Locale.setDefault(Locale.US);
 
 		System.out.println("### Teste 1 - Seller findBYId ###");
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println();
+		
+		System.out.println("### Teste 2 - Seller findBYDepartment ###");
+		Department department = new Department(4, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller s: list) {
+			System.out.println(s);
+		}
+		
+		
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,11 +1,23 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import entities.Department;
 import model.dao.DepartmentDao;
 
 public class DepartmentDaoJDBC implements DepartmentDao{
+	
+	private Connection conn;
+
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Department department) {
@@ -27,8 +39,38 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = conn.prepareStatement(
+					"SELECT a.* "
+					+ " FROM department a"
+					+ " WHERE b.Id = ?");
+			
+			ps.setInt(1, id);
+			
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				Department department = instantiateDepartment(rs);
+				return department;
+			}
+			else return null;			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(ps);
+			DB.closeStatement(ps);
+		}
+	}
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
